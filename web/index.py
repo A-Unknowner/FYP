7958616ba8131a6ab857f67608cb38392d083dcc
im-JonRai <116862321+im-JonRai@users.noreply.github.com>
2024-01-05 21:09:50 +0800

    web

---
+++ web/index.py
@@ -0,0 +1,19 @@
+from flask import (Flask, redirect, render_template, request)
+app = Flask(__name__)
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/search_list", methods=["POST"])
+def search_list():
+    restaurant_name = request.form["restaurant_name"]
+    label = f" {restaurant_name}"
+    return render_template("search_list.html", content = label)
+
+
+if __name__ == "__main__":
+    app.run()
