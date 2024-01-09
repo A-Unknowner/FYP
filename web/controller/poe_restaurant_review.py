af14943a5fd0fdc5f4d08a56bf9008ef0a454da9
tommy08332 <117793492+tommy08332@users.noreply.github.com>
2024-01-09 08:39:38 +0800

    Update poe_restaurant_review.py

--- web/controller/poe_restaurant_review.py
+++ web/controller/poe_restaurant_review.py
@@ -1,4 +1,3 @@
-import poe
 from poe_api_wrapper import PoeApi
 
 import json
@@ -11,7 +10,7 @@ TOKEN = ["GPhGTPtn-7CsrpiZCYUdtw%3D%3D",
          "8kIDzmhtVqfh_2BfhxS5tA%3D%3D"]
 
 """
-ChatGPT Prompt Formula notes:
+ChatGPT Prompt Formula:
 https://www.youtube.com/watch?v=jC4v5AS4RIM
 
 Important
@@ -26,37 +25,41 @@ Optional
 """
 
 message = """
-- You are the researcher of Aspect-Category Sentiment Analysis and you have to classify the given Cantonese restaurant review to the aspects of Food prices, Food quality, Food style options, Restaurant hygiene, Restaurant service, Restaurant general, Restaurant prices, Restaurant location, and Restaurant ambience, and classify these aspects with positive/neutral/negative sentiment, no need to output the result if the review no mentioned. Provide the outputs according to this structure:
-Target Noun: [Original Cantonese text only]
-Target Aspect: [Aspect]
-Target Sentiment: [Sentiment]
-Target Sentence: [Original Cantonese text only]
+- Please classify the given Cantonese restaurant review to the aspects of Food prices, Food quality, Food style options, Restaurant hygiene, Restaurant service, Restaurant general, Restaurant prices, Restaurant location, and Restaurant ambience, and classify these aspects with positive/neutral/negative sentiment, no need to output the result if the review no mentioned. - Please give me the outputs according to this structure:
+- Target Noun: [Original Cantonese text only]
+- Target Aspect: [Aspect]
+- Target Sentiment: [Sentiment]
+- Target Sentence: [Original Cantonese text only]
 
 - Example1:
-Target Noun: 火山排骨
-Target Aspect: Food quality
-Target Sentiment: Positive
-Target Sentence: 火山排骨令到我念念不忘餐廳野食唔錯
+- Target Noun: 火山排骨
+- Target Aspect: Food quality
+- Target Sentiment: Positive
+- Target Sentence: 火山排骨令到我念念不忘餐廳野食唔錯
 
 - Example2:
-Target Noun: 火山排骨
-Target Aspect: Food prices
-Target Sentiment: Positive
-Target Sentence: 火山排骨既量多到嚇親我
+- Target Noun: 火山排骨
+- Target Aspect: Food prices
+- Target Sentiment: Positive
+- Target Sentence: 火山排骨既量多到嚇親我
 
 - Example3:
-Target Noun: 碗碟
-Target Aspect: Restaurant hygiene
-Target Sentiment: Positive
-Target Sentence: 碗碟非常乾淨。
+- Target Noun: 碗碟
+- Target Aspect: Restaurant hygiene
+- Target Sentiment: Positive
+- Target Sentence: 碗碟非常乾淨。
 
 - This is the Cantonese restaurant review:
 """
 
-
+# def analysis_result(input = str(), comments = list()):
 def acsa_result(input = str()):
     # test input 1
-    # input = "海鮮硬既有屎，牛肉發霉，碗碟非常非常非常唔乾淨。甜品是兩件毫無誠意的綠茶糕， 很難食 侍應人手不足，亦欠禮貌，希望管理層看到此投會有所改善。路邊攤小食拼盤有南乳雞、澎湖花枝丸、台灣腸,全部都係台灣既美食。 炸地瓜條 真係好香脆,而且佢落左小小粉,又幾夾下。"
+    # input = """
+    # 海鮮硬既有屎，牛肉發霉，碗碟非常非常非常唔乾淨。
+    # 甜品是兩件毫無誠意的綠茶糕， 很難食 侍應人手不足，亦欠禮貌，希望管理層看到此投會有所改善
+    # 路邊攤小食拼盤有南乳雞、澎湖花枝丸、台灣腸,全部都係台灣既美食。 炸地瓜條 真係好香脆,而且佢落左小小粉,又幾夾下。
+    # """
     
     # test input 2
     #input = "/依間餐廳黎左第二次依到既火山排骨令到我念念不忘餐廳野食唔錯，坐位闊落岩晒一大班人黎食飯.火山排骨，$228上到黎依到既排骨既量多到嚇親我真係堆到好似火山咁既款，依到既排骨肉煮到好淋食落酸酸辣辣既味道，份量就大約3人既量.泰式燒魷魚依到既燒魷魚唔會燒到好老，食落去仲帶有少少脆口配上秘制醬酸酸辣好惹味.椰菜苗（蒜蓉炒）蒜蓉味十足啲菜炒到好有鑊氣.泰式滷水豬手依個豬手有好多醬汁，啲豬手炆到好淋肥瘦適中，好岩送飯"
@@ -93,21 +96,18 @@ def acsa_result(input = str()):
 
         if output != "":
             data = output.split(":")
-            result_key = data[0].replace("-", "").strip()
-            result_value = data[1].replace("-", "").strip()
+            if len(data) == 2:
+                result_key = data[0].replace("-", "").strip()
+                result_value = data[1].replace("-", "").strip()
 
-            match result_key:
-                case "Target Noun":
-                    review["target_word"] = result_value
+                match result_key:
+                    case "Target Noun": review["target_word"] = result_value
 
-                case "Target Aspect":
-                    review["target_aspect"] = result_value
+                    case "Target Aspect": review["target_aspect"] = result_value
 
-                case "Sentiment":
-                    review["sentiment"] = result_value
+                    case "Target Sentiment": review["sentiment"] = result_value
 
-                case "Original Target Sentence":
-                    review["sentence"] = result_value
+                    case "Target Sentence": review["sentence"] = result_value
         else:
             comments.append(review)
             review = dict()
