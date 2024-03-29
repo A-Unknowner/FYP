0a1005d2f122e3078a2ffe9739e77a01eff73ca5
tommy08332 <117793492+tommy08332@users.noreply.github.com>
2024-03-30 00:18:54 +0800

    Update chart_data.py

--- web/controller/chart_data.py
+++ web/controller/chart_data.py
@@ -1,108 +1,5 @@
 from collections import Counter
 
-def find_specific_aspect_polarity(read_csv_data):
-
-    # location
-    location_traffic_convenience = list()
-    location_distance_from_business_district = list()
-    location_easy_to_find = list()
-
-    # service
-    service_wait_time = list()
-    service_waiters_attitude = list()
-    service_parking_convenience = list()
-    service_serving_speed = list()
-
-    # price
-    price_level = list()
-    price_cost_effective = list()
-    price_discount = list()
-
-    # environment
-    environment_decoration = list()
-    environment_noise = list()
-    environment_space = list()
-    environment_cleaness = list()
-
-    # dish
-    dish_portion = list()
-    dish_taste = list()
-    dish_look = list()
-    dish_recommendation = list()
-
-    # others
-    others_overall_experience = list()
-    others_willing_to_consume_again = list()
-
-    for i in range(len(read_csv_data["id"])):
-
-        # location
-        location_traffic_convenience.append(read_csv_data["location_traffic_convenience"][str(i)])
-        location_distance_from_business_district.append(read_csv_data["location_distance_from_business_district"][str(i)])
-        location_easy_to_find.append(read_csv_data["location_easy_to_find"][str(i)])
-
-        # service
-        service_wait_time.append(read_csv_data["service_wait_time"][str(i)])
-        service_waiters_attitude.append(read_csv_data["service_waiters_attitude"][str(i)])
-        service_parking_convenience.append(read_csv_data["service_parking_convenience"][str(i)])
-        service_serving_speed.append(read_csv_data["service_serving_speed"][str(i)])
-
-        # price
-        price_level.append(read_csv_data["price_level"][str(i)])
-        price_cost_effective.append(read_csv_data["price_cost_effective"][str(i)])
-        price_discount.append(read_csv_data["price_discount"][str(i)])
-
-        # environment
-        environment_decoration.append(read_csv_data["environment_decoration"][str(i)])
-        environment_noise.append(read_csv_data["environment_noise"][str(i)])
-        environment_space.append(read_csv_data["environment_space"][str(i)])
-        environment_cleaness.append(read_csv_data["environment_cleaness"][str(i)])
-
-        # dish
-        dish_portion.append(read_csv_data["dish_portion"][str(i)])
-        dish_taste.append(read_csv_data["dish_taste"][str(i)])
-        dish_look.append(read_csv_data["dish_look"][str(i)])
-        dish_recommendation.append(read_csv_data["dish_recommendation"][str(i)])
-
-        # others
-        others_overall_experience.append(read_csv_data["others_overall_experience"][str(i)])
-        others_willing_to_consume_again.append(read_csv_data["others_willing_to_consume_again"][str(i)])
-    
-    
-    # location
-    location_dict = {"location_traffic_convenience":Counter(location_traffic_convenience),
-                      "location_distance_from_business_district":Counter(location_distance_from_business_district),
-                      "location_easy_to_find":Counter(location_easy_to_find)}
-    
-    # service
-    service_dict = {"service_wait_time":Counter(service_wait_time),
-                    "service_waiters_attitude":Counter(service_waiters_attitude),
-                    "service_parking_convenience":Counter(service_parking_convenience),
-                    "service_serving_speed":Counter(service_serving_speed)}
-
-    # price
-    price_dict = {"price_level":Counter(price_level),
-                  "price_cost_effective":Counter(price_cost_effective),
-                  "price_discount":Counter(price_discount)}
-
-    # environment
-    environment_dict = {"environment_decoration":Counter(environment_decoration),
-                        "environment_noise":Counter(environment_noise),
-                        "environment_space":Counter(environment_space),
-                        "environment_cleaness":Counter(environment_cleaness)}
-
-    # dish
-    dish_dict = {"dish_portion":Counter(dish_portion),
-                 "dish_taste":Counter(dish_taste),
-                 "dish_look":Counter(dish_look),
-                 "dish_recommendation":Counter(dish_recommendation)}
-
-    # others
-    others_dict = {"others_overall_experience":Counter(others_overall_experience),
-                   "others_willing_to_consume_again":Counter(others_willing_to_consume_again)}
-
-    return location_dict, service_dict, price_dict, environment_dict, dish_dict, others_dict
-
 
 
 def find_all_polarity_number_and_percentage(read_csv_data):
@@ -332,3 +229,65 @@ def each_aspects_percentage(location_list, service_list, price_list, environment
     return [{"location_percentage" : location_percentage}, {"service_percentage" : service_percentage}, 
             {"price_percentage" : price_percentage}, {"environment_percentage" : environment_percentage}, 
             {"dish_percentage" : dish_percentage}, {"other_percentage" : other_percentage}]
+
+
+
+def each_comment_percentage(read_csv_data):
+
+    each_comment_polarity = list()
+    each_comment_percentage_list = list()
+    
+
+    for i in range(len(read_csv_data["id"])):
+        each_review_data = [
+            read_csv_data["location_traffic_convenience"][str(i)],
+            read_csv_data["location_distance_from_business_district"][str(i)],
+            read_csv_data["location_easy_to_find"][str(i)],
+
+            read_csv_data["service_wait_time"][str(i)],
+            read_csv_data["service_waiters_attitude"][str(i)],
+            read_csv_data["service_parking_convenience"][str(i)],
+            read_csv_data["service_serving_speed"][str(i)],
+
+            read_csv_data["price_level"][str(i)],
+            read_csv_data["price_cost_effective"][str(i)],
+            read_csv_data["price_discount"][str(i)],
+
+            read_csv_data["environment_decoration"][str(i)],
+            read_csv_data["environment_noise"][str(i)],
+            read_csv_data["environment_space"][str(i)],
+            read_csv_data["environment_cleaness"][str(i)],
+
+            read_csv_data["dish_portion"][str(i)],
+            read_csv_data["dish_taste"][str(i)],
+            read_csv_data["dish_look"][str(i)],
+            read_csv_data["dish_recommendation"][str(i)],
+
+            read_csv_data["others_overall_experience"][str(i)],
+            read_csv_data["others_willing_to_consume_again"][str(i)]
+        ]
+
+        each_comment_polarity.append(each_review_data)
+
+    for i, data in enumerate(each_comment_polarity):
+        comment_counter = Counter(data)
+
+        print(comment_counter)
+
+        comment_total = comment_counter[1] + comment_counter[0] + comment_counter[-1]
+
+        print(comment_total)
+
+        positive_value = calculate(comment_counter[1], comment_total)
+
+        neutral_value = calculate(comment_counter[0], comment_total)
+
+        negative_value = calculate(comment_counter[-1], comment_total)
+
+        each_comment_percentage_list.append({"positive_value" : positive_value,
+                                             "neutral_value" : neutral_value,
+                                             "negative_value" : negative_value})
+
+    # print(read_csv_data)
+
+    return each_comment_percentage_list
